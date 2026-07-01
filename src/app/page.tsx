4a3e4bd8a9f6a4f983b9072d5dd15affa7245e09
neev-25 <neevmendapara25@gmail.com>
2026-07-01 17:26:25 +0530

    Force dynamic home page for Vercel serverless deploy.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ import connectDb from "@/lib/connectDB";
 import User from "@/model/user.model";
 import React from "react";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   await connectDb();
   const session = await safeAuth();
